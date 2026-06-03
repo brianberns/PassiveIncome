786322cfe2062822013cc421e4d3018a6628e545
Brian Berns <brianberns@gmail.com>
2026-06-03 11:09:44 -0400

    Modularized.

---
+++ StockTradingBot/NewsFeed.fs
@@ -0,0 +1,50 @@
+﻿namespace StockTradingBot
+
+open System.IO
+open System.Net.Http
+open System.ServiceModel.Syndication
+open System.Xml
+
+type NewsFeed =
+    {
+        Name : string
+        Url : string
+    }
+
+module NewsFeed =
+
+    let create name url =
+        {
+            Name = name
+            Url = url
+        }
+
+    let getItems (httpClient : HttpClient) feed =
+        task {
+            try
+                let! rssXml = httpClient.GetStringAsync(feed.Url)
+                use stringReader = new StringReader(rssXml)
+                use xmlReader = XmlReader.Create(stringReader)
+                let feed = SyndicationFeed.Load(xmlReader)
+                for item in feed.Items do
+                    item.SourceFeed <- feed   // ick
+                return Ok feed.Items
+            with ex ->
+                return Error (feed, ex)
+        } |> Async.AwaitTask
+
+    let feeds =
+        [
+            create
+                "MarketWatch Top Stories"
+                "https://feeds.content.dowjones.io/public/rss/mw_topstories"
+            create
+                "CNBC Top News"
+                "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114"
+            create
+                "CNBC Finance"
+                "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664"
+            create
+                "Yahoo S&P 500"
+                "https://feeds.finance.yahoo.com/rss/2.0/headline?s=%5EGSPC&region=US&lang=en-US"   // ^GSPC = S&P 500
+        ]
--- StockTradingBot/Program.fs
+++ StockTradingBot/Program.fs
@@ -1,63 +1,42 @@
-﻿open System
-open System.IO
+﻿namespace StockTradingBot
+
+open System
 open System.Net.Http
-open System.ServiceModel.Syndication
-open System.Xml
 
-let feedUrls =
-    [
-        "https://feeds.content.dowjones.io/public/rss/mw_topstories"                             // "MarketWatch.com - Top Stories"
-        "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114"   // CNBC: "US Top News and Analysis"
-        "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664"    // CNBC: "Finance"
-        "https://feeds.finance.yahoo.com/rss/2.0/headline?s=%5EGSPC&region=US&lang=en-US"        // "Yahoo! Finance: ^GSPC News" (S&P 500)
-    ]
+module Program =
 
-let getItems (httpClient : HttpClient) (feedUrl : string) =
-    task {
-        try
-            let! rssXml = httpClient.GetStringAsync(feedUrl)
-            use stringReader = new StringReader(rssXml)
-            use xmlReader = XmlReader.Create(stringReader)
-            let feed = SyndicationFeed.Load(xmlReader)
-            for item in feed.Items do
-                item.SourceFeed <- feed   // ick
-            return Ok feed.Items
-        with ex ->
-            return Error $"{feedUrl}: {ex.Message}"
-    } |> Async.AwaitTask
+    do
+            // create HTTP client
+        use httpClient =
+            let client = new HttpClient()
+            client.DefaultRequestHeaders
+                .UserAgent
+                .ParseAdd("StockTradingBot/0.1 (mailto:brianberns@gmail.com)")   // needed to avoid 429 errors from Yahoo
+            client
 
-do
-        // create HTTP client
-    use httpClient =
-        let client = new HttpClient()
-        client.DefaultRequestHeaders
-            .UserAgent
-            .ParseAdd("StockTradingBot/0.1 (mailto:brianberns@gmail.com)")   // needed to avoid 429 errors from Yahoo
-        client
+            // fetch news items
+        let items, errors =
+            NewsFeed.feeds
+                |> Seq.map (NewsFeed.getItems httpClient)
+                |> Async.Parallel
+                |> Async.RunSynchronously
+                |> Array.partitionWith (function
+                    | Ok items -> Choice1Of2 items
+                    | Error error -> Choice2Of2 error)
 
-        // fetch news items
-    let items, errors =
-        feedUrls
-            |> Seq.map (getItems httpClient)
-            |> Async.Parallel
-            |> Async.RunSynchronously
-            |> Array.partitionWith (function
-                | Ok items -> Choice1Of2 items
-                | Error message -> Choice2Of2 message)
+            // log errors
+        for feed, ex in errors do
+            printfn $"Error in {feed.Name} feed: {ex.Message}"
 
-        // log errors
-    for error in errors do
-        printfn $"Error: {error}"
-
-    let items =
-        items
-            |> Seq.concat
-            |> Seq.distinctBy _.Id
-            |> Seq.sortByDescending _.PublishDate
-    for item in items do
-        printfn ""
-        printfn $"{item.SourceFeed.Title.Text}"
-        printfn $"{item.Title.Text}"
-        printfn $"{DateTime.UtcNow - item.PublishDate.UtcDateTime}"
-        if item.Summary <> null then
-            printfn $"{item.Summary.Text}"
+        let items =
+            items
+                |> Seq.concat
+                |> Seq.distinctBy _.Id
+                |> Seq.sortByDescending _.PublishDate
+        for item in items do
+            printfn ""
+            printfn $"{item.SourceFeed.Title.Text}"
+            printfn $"{item.Title.Text}"
+            printfn $"{DateTime.UtcNow - item.PublishDate.UtcDateTime}"
+            if item.Summary <> null then
+                printfn $"{item.Summary.Text}"
