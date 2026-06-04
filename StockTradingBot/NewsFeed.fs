040116b004ef7d218a972bcfe87a39781d35968a
Brian Berns <brianberns@gmail.com>
2026-06-04 09:12:53 -0400

    Polish.

--- StockTradingBot/NewsFeed.fs
+++ StockTradingBot/NewsFeed.fs
@@ -1,5 +1,6 @@
 ﻿namespace StockTradingBot
 
+open System
 open System.IO
 open System.Net.Http
 open System.ServiceModel.Syndication
@@ -9,8 +10,10 @@ type NewsItemFilter = SyndicationItem -> bool
 
 module NewsItemFilter =
 
-    let defaultFilter (item : SyndicationItem) =
+    let hasSummary (item : SyndicationItem) =
         item.Summary <> null
+            && not (String.IsNullOrWhiteSpace(
+                item.Summary.Text))
 
     let applyFilters filters item =
         Seq.forall (fun (filter : NewsItemFilter) ->
@@ -65,19 +68,19 @@ module NewsFeed =
                 "MarketWatch Top Stories"
                 "https://feeds.content.dowjones.io/public/rss/mw_topstories"
                 [
-                    NewsItemFilter.defaultFilter
+                    NewsItemFilter.hasSummary
                     (isPersonal >> not)   // filter out personal finance stories
                 ]
             create
                 "CNBC Top News"
                 "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114"
-                [ NewsItemFilter.defaultFilter ]
+                [ NewsItemFilter.hasSummary ]
             create
                 "CNBC Finance"
                 "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664"
-                [ NewsItemFilter.defaultFilter ]
+                [ NewsItemFilter.hasSummary ]
             create
                 "Yahoo S&P 500"
                 "https://feeds.finance.yahoo.com/rss/2.0/headline?s=%5EGSPC&region=US&lang=en-US"   // ^GSPC = S&P 500
-                [ NewsItemFilter.defaultFilter ]
+                [ NewsItemFilter.hasSummary ]
         ]
