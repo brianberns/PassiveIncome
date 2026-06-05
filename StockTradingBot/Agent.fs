52f9e3b596631ef74edc4f10906259d99072d7bd
Brian Berns <brianberns@gmail.com>
2026-06-05 16:31:57 -0400

    Polish.

--- StockTradingBot/Agent.fs
+++ StockTradingBot/Agent.fs
@@ -5,23 +5,31 @@ open System
 open Microsoft.Extensions.AI
 open Microsoft.Extensions.Configuration
 
+/// Decision-making agent.
 type Agent =
     {
+        /// Gemini.
         GoogleClient : Google.GenAI.Client
+
+        /// .NET wrapper.
         ChatClient : IChatClient
     }
 
+    /// Cleanup.
     member this.Dispose() =
         this.ChatClient.Dispose()
         this.GoogleClient.Dispose()
 
     interface IDisposable with
+
+        /// Cleanup.
         member this.Dispose() = this.Dispose()
 
 module Agent =
 
     let private modelId = "gemini-2.5-flash-lite"
 
+    /// Creates an agent.
     let create (config : IConfiguration) =
         let googleClient =
             new Google.GenAI.Client(
@@ -33,6 +41,8 @@ module Agent =
             ChatClient = chatClient
         }
 
+    /// Prompts the agent to respond with a specific type
+    /// of data.
     let getResultAsync<'t> (prompt : string) agent =
         task {
             try
