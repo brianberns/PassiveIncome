9e86a24ebd323b252e7f33b2bfa6cbf836bb6d47
Brian Berns <brianberns@gmail.com>
2026-06-06 00:53:30 -0400

    Refactored.

--- StockTradingBot/Program.fs
+++ StockTradingBot/Program.fs
@@ -13,8 +13,8 @@ module Program =
         ConfigurationBuilder()
             .AddUserSecrets(assembly)
             .Build()
-
     let agent = Agent.create config
+    let broker = Broker.create config
 
     let httpClient =
         let client = new HttpClient()
@@ -23,49 +23,64 @@ module Program =
             .ParseAdd("StockTradingBot/0.1 (mailto:brianberns@gmail.com)")   // needed to avoid 429 errors from Yahoo
         client
 
-    let run () =
+    let printPortfolio (portfolio : Portfolio) =
+        printfn "Portfolio"
+        printfn $"   Tradable cash: {portfolio.TradableCash}"
+        for (asset, value) in Map.toSeq portfolio.PositionMap do
+            printfn $"   {asset}: {value.Quantity} @ {value.AverageEntryPrice}"
 
-        async {
-            match! MarketOverview.getAsync httpClient agent with
-                | MarketOverviewResult.Success overview ->
-                    printfn $"Trend: {overview.Trend}"
+    let printMarketOverview marketOverview =
+        printfn $"Trend: {marketOverview.Trend}"
+        printfn ""
+        printfn "Candidates:"
+        for candidate in marketOverview.Candidates do
+            printfn ""
+            printfn $"{candidate.Asset.Symbol}"
+            printfn $"{candidate.Reason}"
+
+    let printAssetRecommendation (result : AssetRecommendationResult) =
+        match result with
+            | Success results ->
+                printfn "Recommendations:"
+                for result in results do
                     printfn ""
-                    printfn "Candidates:"
-                    for candidate in overview.Candidates do
-                        printfn ""
-                        printfn $"{candidate.Asset.Symbol}"
-                        printfn $"{candidate.Reason}"
-
-                    let! result =
-                        AssetRecommendation.getAsync httpClient agent overview
                     match result with
-                        | AssetRecommendationResult.Success results ->
-                            printfn ""
-                            printfn "Recommendations:"
-                            for result in results do
-                                printfn ""
-                                match result with
-                                    | Ok reco ->
-                                        printfn $"{reco.Asset.Symbol}: {reco.Action}"
-                                        printfn $"{reco.Reason}"
-                                    | Error (asset, exn) ->
-                                        printfn $"Asset error: {asset}: {exn.Message}"
-                        | AssetRecommendationResult.AgentError exn ->
-                            printfn $"Asset recommendation error: {exn.Message}"
-                | FeedErrors errors ->
-                    for feed, exn in errors do
-                        printfn $"News feed error: {feed.Name}: {exn.Message}"
-                | MarketOverviewResult.AgentError exn ->
-                    printfn $"Market overview error: {exn.Message}"
-        } |> Async.RunSynchronously
+                        | Ok reco ->
+                            printfn $"{reco.Asset.Symbol}: {reco.Action}"
+                            printfn $"{reco.Reason}"
+                        | Error (asset, exn) ->
+                            printfn $"Asset error: {asset}: {exn.Message}"
+            | AgentError exn ->
+                printfn $"Asset recommendation error: {exn.Message}"
 
-    let test () =
-        let broker = Broker.create config
+    let runOverview marketOverview =
         async {
-            let! portfolio = Broker.getPortfolio broker
-            printfn "%A" portfolio
-            let! isOpen = Broker.isMarketOpen broker
-            printfn "%A" isOpen
+            printfn ""
+            printMarketOverview marketOverview
+            let! result =
+                AssetRecommendation.getAsync
+                    httpClient agent
+                    marketOverview.Trend
+                    marketOverview.Candidates
+            printfn ""
+            printAssetRecommendation result
+        }
+
+    let run () =
+        async {
+            match! Broker.getPortfolio broker with
+                | Ok portfolio ->
+                    printPortfolio portfolio
+                    match! MarketOverview.getAsync httpClient agent with
+                        | MarketOverviewResult.Success overview ->
+                            do! runOverview overview
+                        | FeedErrors errors ->
+                            for feed, exn in errors do
+                                printfn $"News feed error: {feed.Name}: {exn.Message}"
+                        | MarketOverviewResult.AgentError exn ->
+                            printfn $"Market overview error: {exn.Message}"
+                | Error exn ->
+                    printfn $"Portfolio error: {exn.Message}"
         } |> Async.RunSynchronously
 
     Console.OutputEncoding <- Text.Encoding.UTF8
