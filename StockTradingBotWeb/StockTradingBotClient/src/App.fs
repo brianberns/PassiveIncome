b20aeee2ad753c040c1b66bf848e1d20bc3cc94f
Brian Berns <brianberns@gmail.com>
2026-06-10 00:21:53 -0400

    Prepared client for Fable Elmish React.

--- StockTradingBotWeb/StockTradingBotClient/src/App.fs
+++ StockTradingBotWeb/StockTradingBotClient/src/App.fs
@@ -1,10 +1,38 @@
-namespace StockTradingBot
+module App
 
-open Browser
+open Feliz
+open Elmish
+open Elmish.React
 
-module App =
+type State = { Count: int }
 
-        // start a session when the browser is ready
-    (~~document).ready(fun () ->
-        let surface = ~~"main"
-        ())
+type Msg =
+    | Increment
+    | Decrement
+
+let init() = { Count = 0 }, Cmd.none
+
+let update (msg: Msg) (state: State) =
+    match msg with
+    | Increment -> { Count = state.Count + 1 }, Cmd.none
+    | Decrement -> { Count = state.Count - 1 }, Cmd.none
+
+let render (state: State) (dispatch: Msg -> unit) =
+    Html.div [
+        Html.button [
+            prop.onClick (fun _ -> dispatch Increment)
+            prop.text "Increment"
+        ]
+
+        Html.button [
+            prop.onClick (fun _ -> dispatch Decrement)
+            prop.text "Decrement"
+        ]
+
+        Html.h1 state.Count
+    ]
+
+Program.mkProgram init update render
+    |> Program.withReactSynchronous "elmish-app"
+    |> Program.withConsoleTrace
+    |> Program.run
